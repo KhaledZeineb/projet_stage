846e8ce6444511a74307d73c030d8f3e01caab03
KhaledZeineb <khaledzeineb81@gmail.com>
2025-01-09 18:26:32 +0100

    Ajout de la fonctionnalité [description]

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#stagiairesTable').DataTable({
+        "paging": true,
+        "pageLength": 5,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.12.1/i18n/fr_fr.json"
+        }
+    });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./*.html","./*.js"], // Ou ajustez le chemin selon vos fichiers
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
