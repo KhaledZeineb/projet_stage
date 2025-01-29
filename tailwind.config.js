979a9de0f1f20ccf01876d813467e7031fc557f2
KhaledZeineb <khaledzeineb81@gmail.com>
2025-01-29 14:47:44 +0100

    Premier commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html","./*.js"], // Ou ajustez le chemin selon vos fichiers
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
 
